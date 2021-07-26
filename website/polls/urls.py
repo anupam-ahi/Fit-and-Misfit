d30b2ae68ee552276024e8ad8899677481e3a5b3
anupam-ahi <anupam.gollapalli@gmail.com>
2021-07-26 18:20:01 +0530

    image tag works!!!!!

--- website/polls/urls.py
+++ website/polls/urls.py
@@ -6,4 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('contact', views.contact, name="contact"),
+    path('recommendations', views.recommendations, name="recommendations"),
+    path('newsletter', views.newsletter, name="newsletter"),
+    path('books', views.books, name="books"),
+    path('articles', views.articles, name="articles"),
+    path('audio_books', views.audio_books, name="audio_books"),
 ]
--- website/polls/views.py
+++ website/polls/views.py
@@ -11,3 +11,23 @@ def home(request):
 
 def contact(request):
     return render(request, "contact.html")
+
+
+def recommendations(request):
+    return render(request, "recommendations.html")
+
+
+def newsletter(request):
+    return render(request, "newsletter.html")
+
+
+def books(request):
+    return render(request, "books.html")
+
+
+def articles(request):
+    return render(request, "articles.html")
+
+
+def audio_books(request):
+    return render(request, "audio_books.html")
