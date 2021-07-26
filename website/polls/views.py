6a9a513f7c2e3fb246bce019c3aeb11639440912
anupam-ahi <anupam.gollapalli@gmail.com>
2021-07-26 17:39:36 +0530

    about page added

--- website/polls/views.py
+++ website/polls/views.py
@@ -1,3 +1,13 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def contact(request):
+    return render(request, "contact.html")
